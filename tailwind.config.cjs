3f24bfbbacb78306a2a45fd8d66ce39f85c7f0f4
“xrischev” <shevchenko93@abv.bg>
2023-02-17 20:43:59 +0200

    'page'

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,43 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+      container: {
+        // you can configure the container to be centered
+        center: true,
+
+        // or have default horizontal padding
+        padding: '1rem',
+
+        // default breakpoints but with 40px removed
+        screens: {
+          sm: '700px',
+          md: '850px',
+          lg: '984px',
+          xl: '1240px',
+          '2xl': '1496px',
+        }
+        },
+         screens: {
+              'sm': '640px',
+              // => @media (min-width: 640px) { ... }
+
+              'md': '769px',
+
+              'lg': '1025px',
+              // => @media (min-width: 1024px) { ... }
+
+              'xl': '1280px',
+              // => @media (min-width: 1280px) { ... }
+
+              '2xl': '1536px',
+              // => @media (min-width: 1536px) { ... }
+            },
+
+    extend: {
+        colors: {
+            'regal-blue': '#243c5a',
+          },
+    }
   },
   plugins: [],
 };
